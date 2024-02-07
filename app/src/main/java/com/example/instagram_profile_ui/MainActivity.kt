ec596cda1f7a2c399f513b73e15505b4557b728e
Youssef_Elfeky <youssefelfeky83@gmail.com>
2024-02-07 07:12:41 +0200

    Initial commit

---
+++ app/src/main/java/com/example/instagram_profile_ui/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.instagram_profile_ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.instagram_profile_ui.ui.ProfileScreen
+import com.example.instagram_profile_ui.ui.theme.Instagram_Profile_UITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ProfileScreen()
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/instagram_profile_ui/ui/ImageWithText.kt
@@ -0,0 +1,8 @@
+package com.example.instagram_profile_ui.ui
+
+import androidx.compose.ui.graphics.painter.Painter
+
+class ImageWithText(
+    val image :Painter,
+    val text:String
+)
